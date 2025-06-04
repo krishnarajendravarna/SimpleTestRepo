e2df8f938661c07f546f72c85c935ba6c50edd4a
User <User@192.168.0.101>
2025-06-04 12:50:53 +0530

    Add multiply method

--- Java_Basic/src/com/basic/Main.java
+++ Java_Basic/src/com/basic/Main.java
@@ -1,23 +1,28 @@
 package com.basic;
 
 class Calculator {
- 
- public int add(int a, int b) {
-     return a + b;
- }
 
- public int subtract(int a, int b) {
-     return a - b;
- }
+	public int add(int a, int b) {
+		return a + b;
+	}
+
+	public int subtract(int a, int b) {
+		return a - b;
+	}
+
+	public int multiply(int a, int b) {
+		return a * b;
+	}
+
 }
 
 public class Main {
- public static void main(String[] args) {
-     Calculator calc = new Calculator();
+	public static void main(String[] args) {
+		Calculator calc = new Calculator();
 
-     int num1 = 10, num2 = 5;
+		int num1 = 10, num2 = 5;
 
-     System.out.println("Addition: " + calc.add(num1, num2));
-     System.out.println("Subtraction: " + calc.subtract(num1, num2));
- }
+		System.out.println("Addition: " + calc.add(num1, num2));
+		System.out.println("Subtraction: " + calc.subtract(num1, num2));
+	}
 }
