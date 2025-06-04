b46a757fcacf435fc2d1528e6582ff3069f998cc
User <User@192.168.0.101>
2025-06-04 11:48:19 +0530

    Adding add and subtract methods in Calculator class.

---
+++ Java_Basic/src/com/basic/Main.java
@@ -0,0 +1,23 @@
+package com.basic;
+
+class Calculator {
+ 
+ public int add(int a, int b) {
+     return a + b;
+ }
+
+ public int subtract(int a, int b) {
+     return a - b;
+ }
+}
+
+public class Main {
+ public static void main(String[] args) {
+     Calculator calc = new Calculator();
+
+     int num1 = 10, num2 = 5;
+
+     System.out.println("Addition: " + calc.add(num1, num2));
+     System.out.println("Subtraction: " + calc.subtract(num1, num2));
+ }
+}
